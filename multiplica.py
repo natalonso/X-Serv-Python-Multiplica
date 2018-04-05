bae262335ac3adc711664d3f990b4c7e8d5277ca
Natalia Alonso Arteaga <nalonso@alpha05.aulas.gsyc.urjc.es>
2018-04-05 16:34:09 +0200

    Programa tablas de multiplicar

---
+++ multiplica.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for numero1 in range(1,11):
+    print("Tabla del ", numero1)
+    for numero2 in range(1,11):
+        resultado=numero1*numero2
+        print(numero1, " por ", numero2, " es:", resultado)
